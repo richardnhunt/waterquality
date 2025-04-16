2caa4fd6898e916ca2471ee3ba2175278f8e8650
Richard Hunt <richard@cooljacketcoding.co.uk>
2025-04-16 06:40:13 +0100

    parse_epicollect: initial commit

---
+++ epicollect/cfg_arag.py
@@ -0,0 +1,137 @@
+# Avonvale River Action Group
+# ===========================
+
+INPUT_CSV_FILENAME = 'form-1__wqmn-water-quality-survey.csv'
+COL_NAME = 4
+COL_DATE = 5
+COL_RIVER = 6
+COL_PLACE = 7
+COL_LAT = 8
+COL_LONG = 9
+COL_ACCURACY = 10
+COL_NORTHING = 11
+COL_EASTING = 12
+COL_UTMZONE = 13
+COL_DESCRIPTION = None
+COL_TIME = 14
+COL_CSO = None
+COL_CONDUCTIVITY = 19
+COL_TEMPERATURE = 20
+COL_NITRATES = 22
+COL_PHOSPHATES = 21
+COL_RIVER_HEIGHT = 15
+COL_NOTES = 26
+COL_RIVER_FLOW = 16
+COL_WEATHER = 17
+COL_RECENT_RAIN = 18
+COL_AMMONIA = 23
+COL_ALGAL_BLOOMS = 24
+COL_POLLUTION = 25
+
+# River names we are monitoring, case insensitive, we use the first one
+RIVER_NAMES = [ ("Warwickshire Avon", "warwickshire avon fladbury", "warwickshire river avon", "warks avon", "River Avon Worcs"),
+                ("Warwickshire Arrow", "Arrow", "Warks Arrow"),
+                ("Alne", "River Alne"),
+                ("Isbourne","Isbourne"),
+                ("Badsey Brook","Badsey Brook"),
+                ("Elmley Brook","Elmley Brook","River Avon tributary brook")
+              ]
+
+# We map some names for consitency, include the location number if there is one (will be then stripped out)
+# Names get changed to the first one in the list
+SYNONYMS = [ ("220 Fladbury, Jubilee Bridge", "fladbury, Jubilee Bridge"),
+             ("220 Fladbury, Jubilee Bridge", "Jubilee Bridge Fladbury"),
+             ("220 Fladbury, Jubilee Bridge", "Jubilee Bridge"),
+             ("270 Pershore, Leisure Centre", "Leisure Centre Pershore", "Pershore Leisure Centre", "Pershore les cent 270", "Pershore Leisure Centre 270"),
+             ("230 Charlton, Merry Brook", "230merry brook"),
+             ("120 Offenham, Dead Mens Ait", "120a offenham Dead Mens Ait (opp)"),
+             ("100 Wootten Warwen", "Pennyford Lane Bridge", "Pennyford Bridge lane", "Pennyford Lane (BAA WATER)", "Pennyford bridge"),
+             ("Lido Stratford upon Avon", "Lido"),
+             ("120 Spernal Lane", "Spernall Lane Bridge", "Spernal Lane Bridge", "spernall", "spernal lane"),
+             ("Cleeve Prior", "103 Cleeve Prior"),
+             ("240 Cropthorne, Holland House", "Holland House", "Holland House site 240"),
+             ("25 Hinton, Bridge","25 Hinton, bridge"),
+             ("30 Hampton, Bridge","30 Hampton, bridge"),
+             ("108 B4510 Bridge", "200 B4510 bridge"),
+             ("245 Cropthorne Main Street, Bridge", "245 Cropthorne Main Street Bridge"),
+             ("280 Pershore, Bridges", "Pershore bridges"),
+             ("Rugby, Steam Turbine", "Rugby; Steam Turbine", "Rugby (Steam Turbine)")
+           ]
+
+# Locations which have a number
+# If we find a number in the place name in spreadsheet, then try to look up that number for the river in this list first
+# Place name in table below must be the same as SYNONYMS above but without the number
+SAMPLING_LOCATIONS = [
+    ("Isbourne", 25, "Hinton, Bridge", 52.063505, -1.967044),
+    ("Isbourne", 30, "Hampton, Bridge", 52.085601, -1.956927),
+    ("Warwickshire Avon", 50, "B4510 bridge", 52.104597, -1.916057),
+    ("Warwickshire Avon", 100, "Bidford Bridge", 52.163627, -1.856484),
+    ("Badsey Brook", 108, "B4510 Bridge", 52.091779, -1.907077),
+    ("Elmley Brook", 245, "Cropthorne Main Street, Bridge", 52.100479, -2.007295),
+    ("Warwickshire Avon", 115, "Twyford ANT landing", 52.117573, -1.930736),
+    ("Warwickshire Avon", 120, "Offenham, Dead Mens Ait", 52.109891, -1.929374),
+    ("Warwickshire Avon", 130, "Evesham, de Montfort bridge", 52.105462, -1.929985),
+    ("Warwickshire Avon", 140, "Evesham, slip above weir", 52.094453, -1.939439),
+    ("Warwickshire Avon", 150, "Evesham, Waterside/Abbey Moorings", 52.09077, -1.94410),
+    ("Warwickshire Avon", 160, "Evesham, Battleton Brook", 52.08820, -1.94588),
+    ("Warwickshire Avon", 165, "Evesham Corporation Meadow", 52.086446, -1.956054),
+    ("Warwickshire Avon", 170, "Evesham Hampton Ferry", 52.09155, -1.95809),
+    ("Warwickshire Avon", 175, "Evesham, Blind Lane", 52.09547, -1.95607),
+    ("Warwickshire Avon", 180, "Evesham, Worcester Rd ATS", 52.102447, -1.953149),
+    ("Warwickshire Avon", 190, "Chadbury, Lock", 52.112619, -1.964094),
+    ("Warwickshire Avon", 200, "Chadbury, Wood Norton", 52.120157, -1.983626),
+    ("Warwickshire Avon", 210, "Fladbury, Paddle Club", 52.118181, -2.006461),
+    ("Warwickshire Avon", 220, "Fladbury, Jubilee Bridge", 52.108816, -2.000528),
+    ("Warwickshire Avon", 230, "Charlton, Merry Brook", 52.10853, -1.98801),
+    ("Warwickshire Avon", 240, "Cropthorne, Holland House", 52.102836, -2.006356),
+    ("Warwickshire Avon", 245, "Cropthorne Main Street Bridge", 52.100434, -2.007293),
+    ("Warwickshire Avon", 250, "Cropthorne, below pump house", 52.103992, -2.020204),
+    ("Warwickshire Avon", 255, "Cropthorne strawberry farm", 52.105278, -2.029053),
+    ("Warwickshire Avon", 260, "Lower Moor, Salters Lane/Springs", 52.11427, -2.03578),
+    ("Warwickshire Avon", 265, "Wyre Piddle", 52.12369, -2.05767),
+    ("Warwickshire Avon", 270, "Pershore, Leisure Centre", 52.112278, -2.071453),
+    ("Warwickshire Avon", 205, "Pershore, Bridges", 52.10525, -2.07059),
+    ("Warwickshire Avon", 280, "Pershore, Bridges", 52.10525, -2.07059),
+    ("Warwickshire Avon", 290, "Pensham", 52.10042, -2.088309),
+    ("Warwickshire Avon", 300, "Eckington, Bridge", 52.078815, -2.11487),
+    ("Warwickshire Avon", 305, "Mary Brook", 52.096218, -2.049696),
+    ("Warwickshire Avon", 310, "Defford, Arden Sailing Club", 52.079476, -2.127854),
+    ("Warwickshire Arrow", 100, "Studley", 52.27419029342931, -1.883789898760419),
+    ("Warwickshire Arrow", 120, "Spernal Lane", 52.26057296331432, -1.8762366844985907),
+    ("Warwickshire Arrow", 200, "Salford Bridge", 52.16338142682303, -1.8803195030184976),
+    ("Alne", 100, "Wootten Warwen (=Pennyford Lane Bridge)", 52.2581988721852, -1.783556704096704),
+    ("Alne", 130, "Little Alne", 52.250200998557055, -1.7935596094704758),
+    ("Alne", 150, "Great Alne (Pelham Lane)", 52.23257559455438, -1.8317339626316056)
+]
+
+# Thresholds for graphs
+THRESHOLD_CONDUCTIVITY = 1000
+THRESHOLD_PHOSPHATES = 0.306
+THRESHOLD_NITRATES = 5
+THRESHOLD_AMMONIA = 0.6
+
+# Maximums for graphs
+MAX_CONDUCTIVITY = 3000
+MAX_TEMPERATURE_C = 30
+MAX_PHOSPHATES = 2
+MAX_NITRATES = 10
+
+
+# OUTPUT CONFIGURATION
+######################
+
+# Graph configuration
+FIG_SIZE_X_INCHES = 1
+FIG_SIZE_Y_INCHES = 0.75
+
+# ...positions as fraction of figure size
+MARGIN_TOP_FRACTION = 0.995
+MARGIN_BOTTOM_FRACTION = 0.11
+MARGIN_LEFT_FRACTION = 0.08
+MARGIN_RIGHT_FRACTION = (1 - MARGIN_LEFT_FRACTION)
+
+YAXIS_LABEL_SPACING = 0.5
+TICK_LABEL_SPACING = 0.5
+SPACE_BETWEEN_SUBPLOTS = 0.1
+
+BAR_WIDTH = 10
---
+++ epicollect/parse_epicollect.py
@@ -0,0 +1,331 @@
+# Parse raw exported sample data from epicollect into graphs for use
+# within a kml file including output of individual tidied .csv files.
+# Different river synonyms and placenames are grouped together during
+# this process.
+#
+# R. Hunt
+# richard@cooljacketcoding.co.uk
+
+import os
+import sys
+import csv
+from collections import defaultdict
+from collections import namedtuple
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+import matplotlib.dates as mdates
+from datetime import datetime
+import numpy as np
+
+
+# SELECT CONFIGURATION
+# ====================
+import cfg_arag as cfg
+
+# Set to 1 for one-off test
+TEST = 0
+
+
+# INTERNAL DATE
+###############
+
+SampleEntry = namedtuple('SampleEntry', 'name date river place latitude longitude accuracy northing easting utmzone location_description time river_height river_flow weather recent_rain reading_conductivity reading_temperature reading_phosphates reading_nitrates reading_ammonia reading_algal_blooms reading_pollution reading_notes')
+
+sampling_data = defaultdict(list)
+
+
+# FUNCTIONS FOR PLOTTING
+########################
+
+# Define a function to calculate global min and max values
+def calculate_global_limits(sampling_data):
+    conductivity_min, conductivity_max = float('inf'), float('-inf')
+    phosphates_min, phosphates_max = float('inf'), float('-inf')
+    nitrates_min, nitrates_max = float('inf'), float('-inf')
+    ammonia_min, ammonia_max = float('inf'), float('-inf')
+    temperature_min, temperature_max = float('inf'), float('-inf')
+    min_date, max_date = datetime.max, datetime.min
+
+    for river, data in sampling_data.items():
+        reading_conductivity = [entry.reading_conductivity for entry in data]
+        reading_phosphates = [entry.reading_phosphates for entry in data]
+        reading_nitrates = [entry.reading_nitrates for entry in data]
+        reading_ammonia = [entry.reading_ammonia for entry in data]
+        reading_temperature = [entry.reading_temperature for entry in data]
+        dates = [datetime.strptime(entry.date, '%d/%m/%Y') for entry in data]
+
+        conductivity_min = min(conductivity_min, *reading_conductivity)
+        conductivity_max = max(conductivity_max, *reading_conductivity)
+
+        phosphates_min = min(phosphates_min, *reading_phosphates)
+        phosphates_max = max(phosphates_max, *reading_phosphates)
+
+        nitrates_min = min(nitrates_min, *reading_nitrates)
+        nitrates_max = max(nitrates_max, *reading_nitrates)
+
+        ammonia_min = min(ammonia_min, *reading_ammonia)
+        ammonia_max = max(ammonia_max, *reading_ammonia)
+
+        temperature_min = min(temperature_min, *reading_temperature)
+        temperature_max = max(temperature_max, *reading_temperature)
+
+        min_date = min(min_date, *dates)
+        max_date = max(max_date, *dates)
+
+    return (conductivity_min, conductivity_max,
+            phosphates_min, phosphates_max,
+            nitrates_min, nitrates_max,
+            ammonia_min, ammonia_max,
+            temperature_min, temperature_max,
+            min_date, max_date)
+
+
+# Define a function to create the plots for each location
+def plot_river_data(location, data, global_limits):
+
+    # Extract data for the location, clipping conductivity data
+    dates = [mdates.date2num(datetime.strptime(entry.date, '%d/%m/%Y')) for entry in data]
+    reading_conductivity = [min(entry.reading_conductivity, cfg.MAX_CONDUCTIVITY) for entry in data]
+    reading_phosphates = [entry.reading_phosphates for entry in data]
+    reading_nitrates = [entry.reading_nitrates for entry in data]
+    reading_ammonia = [entry.reading_ammonia for entry in data]
+    reading_temperature = [entry.reading_temperature for entry in data]
+
+    # Unpack global limits
+    conductivity_min, conductivity_max, phosphates_min, phosphates_max, nitrates_min, nitrates_max, ammonia_min, ammonia_max, temperature_min, temperature_max, min_date, max_date = global_limits
+
+    # Limit conductivity axis
+    if conductivity_max > cfg.MAX_CONDUCTIVITY:
+        conductivity_max = cfg.MAX_CONDUCTIVITY
+
+    if temperature_max > cfg.MAX_TEMPERATURE_C:
+        temperature_max = cfg.MAX_TEMPERATURE_C
+
+    if phosphates_max > cfg.MAX_PHOSPHATES:
+        phosphates_max = cfg.MAX_PHOSPHATES
+
+    if nitrates_max > cfg.MAX_NITRATES:
+        nitrates_max = cfg.MAX_NITRATES
+
+    # Create a figure and subplots (optimize for small size)
+    fig, axs = plt.subplots(2, 1, figsize=(cfg.FIG_SIZE_X_INCHES, cfg.FIG_SIZE_Y_INCHES), dpi=600, sharex=True)
+
+    # Set the font size in pixels (roughly equivalent to 16 pixels)
+    label_fontsize = 8 * (72.27 / fig.dpi)  # Convert 16 pixels to relative fontsize
+    tick_fontsize = 7 * (72.27 / fig.dpi)
+
+    # Define the bar plots for each reading and temperature
+    def create_subplot(ax, y_values, y_label, min_y, max_y, threshold_y, colour_y, y2_values, y2_label, min_y2, max_y2, threshold_y2, colour_y2):
+
+        offset_dates = [(date + cfg.BAR_WIDTH) for date in dates]
+
+        # Primary y-axis: plot the main reading
+        ax.bar(dates, y_values, color=colour_y, width=cfg.BAR_WIDTH, label=y_label)
+        ax.set_ylabel(y_label, fontsize=label_fontsize, labelpad=cfg.YAXIS_LABEL_SPACING)
+        ax.set_ylim(min_y, max_y)
+        ax.tick_params(axis='both', which='major', labelsize=tick_fontsize, width=0.1, length=1, pad=cfg.TICK_LABEL_SPACING)
+
+        # Secondary y-axis: plot the temperature as bars
+        ax_twin = ax.twinx()
+        ax_twin.bar(offset_dates, y2_values, color=colour_y2, width=cfg.BAR_WIDTH, label=y2_label)
+        ax_twin.set_ylabel(y2_label, fontsize=label_fontsize, labelpad=cfg.YAXIS_LABEL_SPACING)
+        ax_twin.set_ylim(min_y2, max_y2)
+        ax_twin.tick_params(axis='y', labelsize=tick_fontsize, width=0.1, length=1, pad=cfg.TICK_LABEL_SPACING)
+
+        # Thresholds
+        if (threshold_y != 0):
+            ax.axhline(y=threshold_y, color=colour_y, linestyle='dotted', linewidth=0.25)
+
+        if (threshold_y2 != 0):
+            ax_twin.axhline(y=threshold_y2, color=colour_y2, linestyle='dotted', linewidth=0.25)
+
+        # Legends for both y-axes
+        ax.legend(loc='upper left', fontsize=label_fontsize)
+        ax_twin.legend(loc='upper right', fontsize=label_fontsize)
+
+        # Format x-axis ticks
+        ax.set_xticks(dates)
+        plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
+        ax.xaxis.set_major_locator(mdates.MonthLocator())  # Major ticks every month
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
+        ax.set_xlim(min_date, max_date)
+        ax.xaxis.set_tick_params(width=0.1, length=1, pad=cfg.TICK_LABEL_SPACING)
+        for spine in ax.spines.values():
+            spine.set_linewidth(0.1)
+        for spine in ax_twin.spines.values():
+            spine.set_linewidth(0.1)
+
+    # Subplots
+    create_subplot(axs[0], reading_conductivity, 'Conductivity uS/cm', conductivity_min, conductivity_max, cfg.THRESHOLD_CONDUCTIVITY, 'orange',
+                   reading_ammonia, 'Ammonia ppm', ammonia_min, ammonia_max, cfg.THRESHOLD_AMMONIA, 'lightblue');
+
+    create_subplot(axs[1], reading_phosphates, 'Phosphates ppm', phosphates_min, phosphates_max, cfg.THRESHOLD_PHOSPHATES, 'red',
+                   reading_nitrates, 'Nitrates ppm', nitrates_min, nitrates_max, cfg.THRESHOLD_NITRATES, 'blue');
+
+    # Margins around all plots
+    plt.subplots_adjust(left=cfg.MARGIN_LEFT_FRACTION, right=cfg.MARGIN_RIGHT_FRACTION, top=cfg.MARGIN_TOP_FRACTION, bottom=cfg.MARGIN_BOTTOM_FRACTION, hspace=cfg.SPACE_BETWEEN_SUBPLOTS)
+
+    fig.suptitle(f'{location}', fontsize=label_fontsize, fontweight='bold')
+
+    # Save the figure as a PNG file with the location name
+    filename = location.replace('/','_')
+    plt.savefig(f'{filename}.png', bbox_inches='tight', pad_inches=0)
+
+    if TEST == 1:
+        plt.show()
+
+    plt.close()
+
+
+# FILE HANDLING
+###############
+def read_river_data(csv_file):
+    with open(csv_file, mode='r', newline='', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        next(reader)
+        # Extract information from row on spreadsheet
+        for row in reader:
+            if cfg.COL_RIVER == None:
+                one_for_us = True
+                river = None
+            else:
+                one_for_us = False
+                river = row[cfg.COL_RIVER].lower().strip()
+                river_cleaned_up = river.lower().strip()
+                for river_name_set in cfg.RIVER_NAMES:
+                    for river_name_monitoring in river_name_set:
+                        if river_name_monitoring.lower().strip() == river_cleaned_up:
+                            one_for_us = True
+                            river = river_name_set[0]
+                            break
+
+            if one_for_us:
+                location_description = '' if cfg.COL_DESCRIPTION == None else row[cfg.COL_DESCRIPTION]
+                name = row[cfg.COL_NAME]
+                date = row[cfg.COL_DATE]
+                place = row[cfg.COL_PLACE]
+                latitude = float(row[cfg.COL_LAT]) if row[cfg.COL_LAT] else None
+                longitude = float(row[cfg.COL_LONG]) if row[cfg.COL_LONG] else None
+                accuracy = int(row[cfg.COL_ACCURACY]) if row[cfg.COL_ACCURACY] else None
+                northing = int(row[cfg.COL_NORTHING]) if row[cfg.COL_NORTHING] else None
+                easting = int(row[cfg.COL_EASTING]) if row[cfg.COL_EASTING] else None
+                utmzone = row[cfg.COL_UTMZONE]
+                time = row[cfg.COL_TIME]
+                river_height = row[cfg.COL_RIVER_HEIGHT]
+                river_flow = '' if cfg.COL_RIVER_FLOW == None else row[cfg.COL_RIVER_FLOW]
+                weather = '' if cfg.COL_WEATHER == None else row[cfg.COL_WEATHER]
+                recent_rain = '' if cfg.COL_RECENT_RAIN == None else row[cfg.COL_RECENT_RAIN]
+                reading_conductivity = float(row[cfg.COL_CONDUCTIVITY]) if row[cfg.COL_CONDUCTIVITY] else 0
+                reading_temperature = float(row[cfg.COL_TEMPERATURE]) if row[cfg.COL_TEMPERATURE] else 0
+                reading_phosphates = float(row[cfg.COL_PHOSPHATES]) if row[cfg.COL_PHOSPHATES] else 0
+                reading_nitrates = float(row[cfg.COL_NITRATES]) if row[cfg.COL_NITRATES] else 0
+                if cfg.COL_AMMONIA == None:
+                    reading_ammonia = 0
+                else:
+                    reading_ammonia = float(row[cfg.COL_AMMONIA]) if row[cfg.COL_AMMONIA] else 0
+                reading_algal_blooms = '' if cfg.COL_ALGAL_BLOOMS == None else row[cfg.COL_ALGAL_BLOOMS]
+                reading_pollution = '' if cfg.COL_POLLUTION == None else row[cfg.COL_POLLUTION]
+                reading_notes = row[cfg.COL_NOTES]
+
+                # Pre-process input placename to map any incorrect spellings
+                place = place.strip()
+                for synonym in cfg.SYNONYMS:
+                    for variant in synonym:
+                        if place.lower().startswith(variant.lower()):
+                            place = synonym[0]
+                            break
+
+                # And now look to see if we map to a specific location
+                place_split = place.split()
+                if place_split[0].isdigit():
+                    # An 'official' location so override name and lat/long from our record for consistency
+                    place_number = int(place_split[0])
+                    for entry in cfg.SAMPLING_LOCATIONS:
+                        if (entry[0] == river) and (entry[1] == place_number):
+                            place = entry[2]
+                            latitude = entry[3]
+                            longitude = entry[4]
+                            break
+                else:
+                    for entry in cfg.SAMPLING_LOCATIONS:
+                        if (place == entry[2]):
+                            latitude = entry[3]
+                            longitude = entry[4]
+                            break
+
+                # Store processed sample
+                location = place if river == None else river + ', ' + place
+                sample_entry = SampleEntry(
+                    name, date, river, place, latitude, longitude, accuracy, 
+                    northing, easting, utmzone, location_description, time, river_height, river_flow, 
+                    weather, recent_rain, reading_conductivity, reading_temperature, 
+                    reading_phosphates, reading_nitrates, reading_ammonia, reading_algal_blooms, 
+                    reading_pollution, reading_notes
+                )
+                sampling_data[location].append(sample_entry)
+
+def main():
+
+    # Read in data and find largest range so can be common across all graphs
+    print('Reading in river data')
+    read_river_data(cfg.INPUT_CSV_FILENAME)
+    global_limits = calculate_global_limits(sampling_data)
+
+    # Create the graphs of all sampling locations
+    print('Creating graphs')
+    if TEST == 1:
+        location, data = next(iter(sampling_data.items()))
+        plot_river_data(location, data, global_limits)
+    else:
+        for location, data in sampling_data.items():
+            print (location)
+            plot_river_data(location, data, global_limits)
+
+    # Create a kml file to reference the above from
+    print('Generating Google Earth .kml file')
+    num_graphs = 0
+    num_added_to_kml = 0
+    with open('river_overview.kml', 'w') as file:
+        file.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+        file.write('<kml xmlns="http://www.opengis.net/kml/2.2">\n')
+        file.write('  <Document>\n')
+        file.write('    <name>River sampling results</name>\n')
+        for location, data in sampling_data.items():
+            for sample in data:
+                latitude = sample[4]
+                longitude = sample[5]
+                if latitude != None and longitude != None:
+                    break
+            num_graphs += 1
+            filename = location.replace('/','_')
+            if latitude != None and longitude != None:
+                file.write('        <Placemark>\n')
+                file.write('          <name></name>\n')
+                file.write('          <description><![CDATA[\n')
+                file.write('            <img src="' + filename + '.png" alt="' + filename + '" width="594" height="446">\n')
+                file.write('          ]]></description>\n')
+                file.write('          <Point>\n')
+                file.write('            <coordinates>' + str(longitude) + ',' + str(latitude) + ',0</coordinates>\n')
+                file.write('          </Point>\n')
+                file.write('        </Placemark>\n')
+                num_added_to_kml += 1
+            else:
+                print('No location for '+location);
+        file.write('  </Document>\n')
+        file.write('</kml>\n')
+    print('Created ' + str(num_graphs) + ' graphs and linked ' + str(num_added_to_kml) + ' into kml file')
+
+    # Output cleaned csv
+    print('Generating .csv files')
+    for location, data in sampling_data.items():
+        filename = location.replace('/','_') + '.csv'
+        with open(filename, 'w') as file:
+            file.write('name,date,river,place,latitude,longitude,accuracy,location_description,time,river_height,river_flow,weather,recent_rain,reading_conductivity,reading_temperature,reading_phosphates,reading_nitrates,reading_ammonia,reading_algal_blooms,reading_pollution,reading_notes\n')
+            for sample in data:
+                # exclude northing, easting, and utmzone present in sample tuple, add quotes to place and reading notes
+                filtered_entry = sample[:3] + (f'"{sample[3]}"',) + sample[4:7] + (f'"{sample[10]}"',) + sample[11:-1] + (f'"{sample[-1]}"',)
+                file.write(','.join(map(str, filtered_entry)) + '\n')
+
+
+if __name__ == "__main__":
+    main()
